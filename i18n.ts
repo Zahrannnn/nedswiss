b9b47d2000444f45716145a6cd687c3b9c688cb7
ZAHRAN88 <palastinearabia@gmail.com>
2025-10-21 02:06:09 +0300

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,7 +1,7 @@
 export const locales = ['en', 'de', 'fr'] as const;
 export type Locale = (typeof locales)[number];
 
-export const defaultLocale: Locale = 'en';
+export const defaultLocale: Locale = 'de';
 
 export function getDirection(locale: Locale) {
   return 'ltr';
